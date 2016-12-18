850395e0291fc4284636248e7a1abe3905396b42
Stefan Misch <misel@misel.de>
2016-12-18 21:17:41 +0100

    reading XML online

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 'use strict';
 
 var config = require('config');
-
 var blogsource = config.get('blogsource');
 
+console.log('#####################################################################################################');
 console.log(blogsource);
+
+var http   = require('http');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+
+
+var request = http.get(blogsource, function(result){
+    var xml = '';
+    result.on('data', function(chunk){
+        xml += chunk;
+    });
+
+    result.on('end', function(){
+        console.log(xml);
+        var json = parser.parseString(xml);
+        //console.log(json);
+    });
+
+
+});
+
+request.on('error', function(e){
+    console.log('Error: ' + e.message);
+});
